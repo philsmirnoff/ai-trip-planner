6286226140931bc19092fa21bedf1c2a7647aa56
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-08-17 21:17:00 -0400

    Add Hero page

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,7 +1,18 @@
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='flex flex-col items-center mx-56 gap-9'>
+      <h1 className='font-extrabold text-[60px] text-center'>
+      <span className='text-[#f56551]'>Discover Your Next Adventure with AI:</span> Personalized Itineraries at Your Fingertips</h1>
+      <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
+      <Link to={'/create-trip'}>
+        <Button> Get Started, It&apos;s Free </Button>
+      </Link>
+
+      <img src='/landing.png' className='' />
+    </div>
   )
 }
 
